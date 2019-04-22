e1bbc9218ed181905b55650fb24626723f73762c
Leila Weiss <leila.weiss@powerhrg.com>
2019-04-22 08:37:50 -0400

    Done.

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -104,7 +104,7 @@ describe 'Movie' do
       end
 
       it 'can get size of the database' do
-        movies_size = can_get_length_of_movie_table
+        movies_size = can_get_size_of_the_database
         expect(movies_size).to eq(5)
       end
 
@@ -153,5 +153,5 @@ describe 'Movie' do
         expect(Movie.all.size).to eq(0)
       end
     end
-  end
+end
 end
